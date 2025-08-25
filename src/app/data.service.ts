15945158033b61883f043aa91e6d4e95b5fb3e21
Aarti <119279339+aartiwamane@users.noreply.github.com>
2025-08-25 11:19:32 +0530

    Initial commit with Angular CRUD project

---
+++ src/app/data.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private apiUrl = 'http://localhost:3000/Employee';
+
+  constructor(private http: HttpClient) {}
+
+  getEmployees(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  addEmployee(emp: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, emp);
+  }
+
+  deleteEmployee(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+  updateEmployee(id: string, empData: any) {
+  return this.http.put(`http://localhost:3000/employees/${id}`, empData);
+}
+
+}
---
+++ src/app/data/data.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-data',
+  templateUrl: './data.component.html',
+  styleUrls: ['./data.component.css']
+})
+export class DataComponent implements OnInit {
+  employees: any[] = [];
+  newEmp = { name: '', position: '', department: '' };
+
+  isEditing = false;
+  editEmpId: string | null = null;
+  private apiUrl = 'http://localhost:3000/Employee';
+
+  // Make http a class property by adding `private`
+  constructor(private empService: DataService, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  // Fetch employees
+  getEmployees() {
+    this.empService.getEmployees().subscribe((data: any[]) => {
+      this.employees = data;
+    });
+  }
+
+  // Add Employee
+  addEmployee(): void {
+    this.empService.addEmployee(this.newEmp).subscribe((response: any) => {
+      this.employees.push({ ...this.newEmp }); // Assuming backend returns the added employee
+      this.newEmp = { name: '', position: '', department: '' };
+    });
+  }
+
+  // Delete Employee using HttpClient directly
+  deleteEmployee(id: string) {
+    if (confirm('Are you sure you want to delete this employee?')) {
+      this.http.delete(`${this.apiUrl}/${id}`).subscribe(() => {
+        this.getEmployees(); // Refresh list after delete
+      });
+    }
+  }
+
+   editEmployee(emp: any) {
+  this.isEditing = true;
+  this.editEmpId = emp._id;
+  this.newEmp = { name: emp.name, position: emp.position, department: emp.department };
+}
+
+updateEmployee() {
+  if (this.editEmpId) {
+    this.empService.updateEmployee(this.editEmpId, this.newEmp).subscribe(() => {
+      this.getEmployees();
+      this.isEditing = false;
+      this.editEmpId = null;
+      this.newEmp = { name: '', position: '', department: '' };
+    });
+  }
+}
+cancelEdit() {
+    this.isEditing = false;
+    this.editEmpId = null;
+    this.newEmp = { name: '', position: '', department: '' };
+  }
+}
